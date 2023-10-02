ec089ce4d60c76a0d202ab569a7d686d7c81a507
dgl1797 <digiacomoluca1797@gmail.com>
2023-10-02 19:12:59 +0200

    wrapper setup

--- src/MoeLia.jl
+++ src/MoeLia.jl
@@ -1,5 +1,13 @@
 module MoeLia
 
-greet() = print("Hello World!")
+struct Solutions
+  first_frontier::Array{AbstractFloat,1}
+  second_frontier::Array{AbstractFloat,1}
+  third_frontier::Array{AbstractFloat,1}
+end # chiedere quali tipi di soluzioni possono esistere
+
+struct Algorithm
+  result::Solutions
+end
 
 end # module MoeLia
